eb4f5a089f53ec42c44ba34ce512061833187247
Vincent Lamouroux <skadprog@gmail.com>
2013-12-19 14:12:06 +0100

    bump

--- Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/MainActivity.java
+++ Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/MainActivity.java
@@ -1,34 +1,50 @@
 package com.skad.android.androidm2ihm;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DummyFragment dummyFragment = new DummyFragment();
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new DummyFragment())
+                    .add(R.id.container, dummyFragment)
                     .commit();
         }
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -46,6 +62,19 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Carefull! This ouputs a lot of data as it gets called reaaaaally ofte
+        Log.d(TAG, "Event[0]: " + Float.toString(event.values[0]));
+        Log.d(TAG, "Event[1]" + Float.toString(event.values[1]));
+        Log.d(TAG, "Event[2]" + Float.toString(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
     /**
      * A dummy fragment containing a simple view.
      */
@@ -56,7 +85,7 @@ public class MainActivity extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
