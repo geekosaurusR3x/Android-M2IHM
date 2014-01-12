05ac87eabb8aef99f85d7484a7c0611d7508f993
Philipp Schmitt <philipp@schmitt.co>
2014-01-12 19:25:56 +0100

    Remove duplicate forward method

--- Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/model/Bullet.java
+++ Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/model/Bullet.java
@@ -6,7 +6,7 @@ package com.skad.android.androidm2ihm.model;
 public class Bullet extends SpriteObject {
     private double mDirX;
     private double mDirY;
-    private int mVelocity;
+    private int mVelocity = 50;
 
     private int mTimeToLive = 100;
 
@@ -24,11 +24,6 @@ public class Bullet extends SpriteObject {
 
     public void setVelocity(int velocity) {
         this.mVelocity = velocity;
-    }
-
-    public void forward() {
-        x += mVelocity * mDirX;
-        y += mVelocity * mDirY;
     }
 
     public void decreaseVelocity() {
