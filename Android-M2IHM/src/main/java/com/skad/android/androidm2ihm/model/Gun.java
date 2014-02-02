6286f0056cb106f1ec92e726334681ba78d8fa04
Vincent Lamouroux <skadprog@gmail.com>
2014-02-02 14:56:53 +0100

    Commenting the Gun

--- Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/model/Gun.java
+++ Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/model/Gun.java
@@ -7,14 +7,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Represent the gun witch fire bullet
+ * Extend SpriteObject
  * Created by skad on 27/12/13.
  */
 public class Gun extends SpriteObject {
+    /**
+     * Las time were the gun fire
+     */
     private long mLastTimeFired = 0;
+    /**
+     * list of the fired bullet
+     */
     private List<Bullet> mBulletList;
+    /**
+     * sprite for the bullet
+     */
     private Bitmap mBulletSprite;
+    /**
+     * Time between two fire
+     */
     private int mFireRate = 1000;
 
+    /**
+     * Constructor.
+     * Generate the Fired Bullet List
+     *
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param fireRate
+     */
     public Gun(int x, int y, int width, int height, int fireRate) {
         super(x, y, width, height);
         mBulletList = new ArrayList<Bullet>();
@@ -54,6 +78,12 @@ public class Gun extends SpriteObject {
         this.mLastTimeFired = lastTimeFired;
     }
 
+    /**
+     * Generate a new bullet and adding it into the BulletList
+     *
+     * @param targetX
+     * @param targetY
+     */
     public void fire(int targetX, int targetY) {
         Bullet bullet = new Bullet(getBoundingRectangle().centerX(), getBoundingRectangle().centerY(), (int) (32 * mRatioWidth), (int) (32 * mRatioHeight));
         bullet.setDir(targetX, targetY);
@@ -62,12 +92,22 @@ public class Gun extends SpriteObject {
         mBulletList.add(bullet);
     }
 
+    /**
+     * Remove the bullet from the Bullet List
+     *
+     * @param bullet
+     */
     public void removeBullet(Bullet bullet) {
         mBulletList.remove(bullet);
     }
 
+    /**
+     * Take the super.toString and add the firerate.
+     *
+     * @return
+     */
     @Override
-    public String toString(){
-        return super.toString()+"/"+getFireRate();
+    public String toString() {
+        return super.toString() + "/" + getFireRate();
     }
 }
