cd85e68ebfee41c6ecd9117286761fda976365ed
Vincent Lamouroux <skadprog@gmail.com>
2014-02-02 18:18:59 +0100

    Removing ScreenOrientation because Useless class Commenting ZipUtils

--- Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/utils/ZipUtils.java
+++ Android-M2IHM/src/main/java/com/skad/android/androidm2ihm/utils/ZipUtils.java
@@ -1,6 +1,7 @@
 package com.skad.android.androidm2ihm.utils;
 
 /**
+ * Utilites class for Ziping / Unziping file
  * Created by skad on 21/01/14.
  */
 
@@ -18,29 +19,43 @@ public class ZipUtils {
     private ZipUtils() {
     }
 
+    /**
+     * Adding File into ZipFile
+     *
+     * @param File
+     * @param zipFile
+     * @throws IOException
+     */
     public static void zip(String File, String zipFile) throws IOException {
-            BufferedInputStream origin = null;
-            FileOutputStream dest = new FileOutputStream(zipFile);
+        BufferedInputStream origin = null;
+        FileOutputStream dest = new FileOutputStream(zipFile);
 
-            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
+        ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
 
-            byte data[] = new byte[BUFFER];
+        byte data[] = new byte[BUFFER];
 
-            Log.d("Compress", "Adding: " + File);
-            FileInputStream fi = new FileInputStream(File);
-            origin = new BufferedInputStream(fi, BUFFER);
-            ZipEntry entry = new ZipEntry(File.substring(File.lastIndexOf("/") + 1));
-            out.putNextEntry(entry);
-            int count;
-            while ((count = origin.read(data, 0, BUFFER)) != -1) {
-                out.write(data, 0, count);
-            }
-            origin.close();
+        Log.d("Compress", "Adding: " + File);
+        FileInputStream fi = new FileInputStream(File);
+        origin = new BufferedInputStream(fi, BUFFER);
+        ZipEntry entry = new ZipEntry(File.substring(File.lastIndexOf("/") + 1));
+        out.putNextEntry(entry);
+        int count;
+        while ((count = origin.read(data, 0, BUFFER)) != -1) {
+            out.write(data, 0, count);
+        }
+        origin.close();
 
-            out.close();
+        out.close();
 
     }
 
+    /**
+     * UnZip zipname into path if exist
+     *
+     * @param path
+     * @param zipname
+     * @throws IOException
+     */
     public static void unpackZip(String path, String zipname) throws IOException {
         InputStream in = new FileInputStream(path + File.separator + zipname);
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(in));
